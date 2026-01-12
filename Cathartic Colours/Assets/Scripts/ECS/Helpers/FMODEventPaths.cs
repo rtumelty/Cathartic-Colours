589c37999942247ea16d37ea3c89956e823f9785
Ronan Tumelty <ronan.tumelty@unity3d.com>
2026-01-12 15:50:07 +0000

    Switch FMOD event path declarations to properties as they cannot be declared AOT

--- Cathartic Colours/Assets/Scripts/ECS/Helpers/FMODEventPaths.cs
+++ Cathartic Colours/Assets/Scripts/ECS/Helpers/FMODEventPaths.cs
@@ -4,10 +4,10 @@ namespace ECS.Audio
 {
     public static class FMODEventPaths
     {
-        public static readonly FixedString64Bytes BlockMove = new FixedString64Bytes("event:/Block_Move");
-        public static readonly FixedString64Bytes BlockMergeSmall = new FixedString64Bytes("event:/Block_Merge_Small");
-        public static readonly FixedString64Bytes BlockMergeMedium = new FixedString64Bytes("event:/Block_Merge_Medium");
-        public static readonly FixedString64Bytes BlockMergeLarge = new FixedString64Bytes("event:/Block_Merge_Large");
-        public static readonly FixedString64Bytes GameOver = new FixedString64Bytes("event:/GameOver");
+        public static FixedString64Bytes BlockMove => "event:/Block_Move";
+        public static FixedString64Bytes BlockMergeSmall => "event:/Block_Merge_Small";
+        public static FixedString64Bytes BlockMergeMedium => "event:/Block_Merge_Medium";
+        public static FixedString64Bytes BlockMergeLarge => "event:/Block_Merge_Large";
+        public static FixedString64Bytes GameOver => "event:/GameOver";
     }
 }
